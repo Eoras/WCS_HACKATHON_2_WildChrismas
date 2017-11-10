58b883e93a6aff2aaa5c9e49897098349809e3e4
Eoras <pauldsb@outlook.fr>
2017-11-10 09:14:24 +0100

    Fonctionnalité de suppression

--- src/WCSBundle/Controller/LivraisonController.php
+++ src/WCSBundle/Controller/LivraisonController.php
@@ -8,25 +8,57 @@
 
 namespace WCSBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use WCSBundle\Entity\Child;
+use WCSBundle\Repository\ChildRepository;
 use WCSBundle\Repository\continentRepository;
+use WCSBundle\WCSBundle;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 
 
 class LivraisonController extends Controller
 {
     /**
-     * @Route("/Livraison")
+     * @Route("/Livraison", name="livraison")
      */
     public function indexAction()
     {
+        if (!empty($_POST['id'])) {
+
+            $repository = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('WCSBundle:Child');
+
+            $child = $repository->find($_POST['id']);
+
+            $form = $this->createDeleteForm($child);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($child);
+            $em->flush();
+
+
+            return $this->redirectToRoute('livraison');
+        }
+
         $continentManager = $this->getDoctrine()->getManager();
         $continents = $continentManager->getRepository('WCSBundle:Continent')->findAll();
 
         return $this->render('WCSBundle:Livraison:livraison.html.twig',[
             'continents' => $continents,
         ]);
+    }
+
+    private function createDeleteForm(Child $child)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('child_delete', array('id' => $child->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+            ;
     }
 
 }
