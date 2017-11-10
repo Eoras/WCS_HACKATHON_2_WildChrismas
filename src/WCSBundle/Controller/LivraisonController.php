20540c976d05ca4e365459ab65ff9062a36d7f9a
Eoras <pauldsb@outlook.fr>
2017-11-10 09:57:53 +0100

    Session pour la partie administration

--- src/WCSBundle/Controller/LivraisonController.php
+++ src/WCSBundle/Controller/LivraisonController.php
@@ -16,7 +16,7 @@ use WCSBundle\Repository\ChildRepository;
 use WCSBundle\Repository\continentRepository;
 use WCSBundle\WCSBundle;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-
+use Symfony\Component\HttpFoundation\Session\Session;
 
 
 class LivraisonController extends Controller
@@ -26,35 +26,27 @@ class LivraisonController extends Controller
      */
     public function indexAction()
     {
+        $session = new Session();
         $password = 'test';
-        if (empty($_POST['submit']) or $_POST['pass'] != $password) {
-            $error = "Tu n'es pas le Père Noël !";
-            return $this->render('WCSBundle:Livraison:admin.html.twig', [
-                'error' => $error,
-            ]);
-        }
-        else {
+        if (NULL !== $session->get("connect")) {
 
-        if (!empty($_POST['id'])) {
+            if (!empty($_POST['id'])) {
 
-            $repository = $this->getDoctrine()
-                ->getManager()
-                ->getRepository('WCSBundle:Child');
+                $repository = $this->getDoctrine()
+                    ->getManager()
+                    ->getRepository('WCSBundle:Child');
 
-            $child = $repository->find($_POST['id']);
+                $child = $repository->find($_POST['id']);
 
-            $form = $this->createDeleteForm($child);
+                $form = $this->createDeleteForm($child);
 
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($child);
-            $em->flush();
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($child);
+                $em->flush();
 
 
-            return $this->redirectToRoute('livraison');
-        }
-
-        $continentManager = $this->getDoctrine()->getManager();
-        $continents = $continentManager->getRepository('WCSBundle:Continent')->findAll();
+                return $this->redirectToRoute('livraison');
+            }
 
             $continentManager = $this->getDoctrine()->getManager();
             $continents = $continentManager->getRepository('WCSBundle:Continent')->findAll();
@@ -62,15 +54,36 @@ class LivraisonController extends Controller
             return $this->render('WCSBundle:Livraison:livraison.html.twig', [
                 'continents' => $continents,
             ]);
+
+        } elseif (!empty($_POST['pass']) and ($_POST['pass'] == $password)) {
+            $session->set('connect', 'connect');
+            return $this->redirectToRoute('livraison');
+
+        } else {
+            $error = "Tu n'es pas le Père Noël !";
+            return $this->render('WCSBundle:Livraison:admin.html.twig', [
+                'error' => $error,
+            ]);
         }
     }
+
     private function createDeleteForm(Child $child)
     {
         return $this->createFormBuilder()
             ->setAction($this->generateUrl('child_delete', array('id' => $child->getId())))
             ->setMethod('DELETE')
-            ->getForm()
-            ;
+            ->getForm();
+    }
+
+    /**
+     * @Route("/Deconnection", name="deconnection")
+     */
+    public function disconnectAction()
+    {
+        $session = new Session();
+        $session->clear();
+        return $this->redirectToRoute('livraison');
+
     }
 
 }
